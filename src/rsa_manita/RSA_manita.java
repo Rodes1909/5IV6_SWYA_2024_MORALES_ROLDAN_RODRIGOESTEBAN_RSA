e88165138ae0fb82a7a6acae4c21a5bad78d609c
Rodes1909 <rmoralesr2101@alumno.ipn.mx>
2023-11-21 00:31:27 -0600

    first commit

---
+++ src/rsa_manita/RSA_manita.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rsa_manita;
+
+import INTERFACES_GUI.Main;
+
+/**
+ *
+ * @author gta6b
+ */
+public class RSA_manita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+        main.setLocationRelativeTo(null);
+
+    }
+    
+}
